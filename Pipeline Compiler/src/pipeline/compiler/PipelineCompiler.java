4b665e591de4d2e29e3e86b9b54624882574ac8d
Esteban Calvo V <estebanedcv@gmail.com>
2017-09-10 00:10:27 -0600

    Se agrega nops por medio de software. De momento los branches están rotos y faltan instrucciones de agregar.

--- Pipeline Compiler/src/pipeline/compiler/PipelineCompiler.java
+++ Pipeline Compiler/src/pipeline/compiler/PipelineCompiler.java
@@ -21,113 +21,220 @@ import java.util.regex.Matcher;
  */
 public class PipelineCompiler {
         
-        private static String agregarCeros(String dato, int largo)
-        {
-            for (int i = dato.length(); i < largo; i++)
-                {
-                    dato = "0".concat(dato);
-                }
-            return dato;
-        }
+    private static String agregarCeros(String dato, int largo)
+    {
+        for (int i = dato.length(); i < largo; i++)
+            {
+                dato = "0".concat(dato);
+            }
+        return dato;
+    }
     
-        public static void main( String args[] ) throws IOException {
-            
-            String fileString = "C:\\Users\\Pelo\\Google Drive\\2017 Semestre 2\\Arquitectura de Computadoras 1\\Proyectos\\Repositorio Proyecto 1\\Pipeline Compiler\\Ejemplos\\assembly.asm";
-            FileReader file = new FileReader(fileString);
-            String codeString = "C:\\Users\\Pelo\\Google Drive\\2017 Semestre 2\\Arquitectura de Computadoras 1\\Proyectos\\Repositorio Proyecto 1\\Pipeline Compiler\\Ejemplos\\code.mcn";
-            FileWriter codedFile = new FileWriter(codeString);
-            BufferedReader br1 = new BufferedReader(file);
-            String line = null;
-            Map<String, Integer> tags = new HashMap<String, Integer>();
-            int contadorTags = 0;
-            while ((line = br1.readLine()) != null)
+    private static String addNops(int[] array, int registroAfectado1, int registroAfectado2)
+    {
+        String stringConNops1 = "";
+        String stringConNops2 = "";
+        int contador1 = 0;
+        int contador2 = 0;
+        while(true)
+        {
+            stringConNops1 = stringConNops1.concat("0000000000000000000\n");
+            if (array[contador1] == registroAfectado1)
             {
-                String pattern = "[a-zA-Z]+:";
-                Pattern r = Pattern.compile(pattern);
-                Matcher m = r.matcher(line);
-                if (m.find())
-                {
-                    tags.put(line.split(":")[0], contadorTags);
-                }
-                contadorTags++;
+                break;
             }
-            br1.close();
-            BufferedReader br2 = new BufferedReader(new FileReader(fileString));
-            while ((line = br2.readLine()) != null)
+            if (contador1 == array.length - 1)
             {
-                String codedInstruction = "";
-                String[] instruction1 = line.split(" ");
-                if (instruction1[0].equals("lb"))
-                {
-                   String dato1 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[1])),4);
-                   String dato2 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   String dato3 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[3])),4);
-                   codedInstruction = codedInstruction.concat("1000").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
-                   codedFile.write(codedInstruction);
-                }
-                if (instruction1[0].equals("add"))
-                {
-                   String dato1 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[1])),4);
-                   String dato2 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   String dato3 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[3])),4);
-                   codedInstruction = codedInstruction.concat("0001").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
-                   codedFile.write(codedInstruction);
-                }
-                if (instruction1[0].equals("sub"))
-                {
-                   String dato1 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[1])),4);
-                   String dato2 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   String dato3 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[3])),4);
-                   codedInstruction = codedInstruction.concat("0010").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
-                   codedFile.write(codedInstruction);
-                }
-                if (instruction1[0].equals("mul"))
-                {
-                   String dato1 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[1])),4);
-                   String dato2 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   String dato3 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[3])),4);
-                   codedInstruction = codedInstruction.concat("0011").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
-                   codedFile.write(codedInstruction);
-                }
-                if (instruction1[0].equals("bne"))
-                {
-                   String dato1 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[1])),4);
-                   String dato2 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   String dato3 = agregarCeros(Integer.toBinaryString(tags.get(instruction1[3])),7);
-                   codedInstruction = codedInstruction.concat("0100").concat(dato1).concat(dato2).concat(dato3).concat("\n");
-                   codedFile.write(codedInstruction);
-                }
-                if (instruction1[0].equals("addi"))
-                {
-                   String dato1 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[1])),4);
-                   String dato2 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   String dato3 = agregarCeros(Integer.toBinaryString(Integer.parseInt(instruction1[2])),4);
-                   codedInstruction = codedInstruction.concat("1010").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
-                   codedFile.write(codedInstruction);
-                }
+                stringConNops1 = "";
+                contador1 = 0;
+                break;
             }
-            codedFile.close();
+            contador1++;
+        }
+        while(true)
+        {
+            stringConNops2 = stringConNops2.concat("0000000000000000000\n");
+            if (array[contador2] == registroAfectado2)
+            {
+                break;
+            }
+            if (contador2 == array.length - 1)
+            {
+                stringConNops2 = "";
+                contador2 = 0;
+                break;
+            }
+            contador2++;
+        }
+        if (contador1>contador2)
+        {
+            return stringConNops1;
+        }
+        else
+        {
+            return stringConNops2;
+        }
+    }
 
-          
+    private static int[] shiftArray(int[] array, int nuevo)
+    {
+        for (int i = 0; i + 1 < array.length; i++)
+        {
+            array[i] = array[i+1];
+        }
+        array[array.length - 1] = nuevo;
+        return array;
+    }
 
-/*
-// String to be scanned to find the pattern.
-      String line = "ADD -1 2 4"
-              +"\nMOV 1 2";
-      String pattern = "ADD -?[0-9]+ -?[0-9]+ -?[0-9]+";
+    public static void main( String args[] ) throws IOException {
 
-      // Create a Pattern object
-      Pattern r = Pattern.compile(pattern);
+        int[] arrayNops = new int[]{17,17,17};
 
-      // Now create matcher object.
-      Matcher m = r.matcher(line);
-      if (m.find( )) {
-         System.out.println("Found value: " + m.group(0) );
-         String reemplazado = m.replaceAll("1001");
-         System.out.println(reemplazado);
-      }else {
-         System.out.println("NO MATCH");
-      }
-*/
+        String fileString = "C:\\Users\\Pelo\\Google Drive\\2017 Semestre 2\\Arquitectura de Computadoras 1\\Proyectos\\Repositorio Proyecto 1\\Pipeline Compiler\\Ejemplos\\assembly.asm";
+        FileReader file = new FileReader(fileString);
+        String codeString = "C:\\Users\\Pelo\\Google Drive\\2017 Semestre 2\\Arquitectura de Computadoras 1\\Proyectos\\Repositorio Proyecto 1\\Pipeline Compiler\\Ejemplos\\code.mcn";
+        FileWriter codedFile = new FileWriter(codeString);
+        BufferedReader br1 = new BufferedReader(file);
+        String line = null;
+        Map<String, Integer> tags = new HashMap<String, Integer>();
+        int contadorTags = 0;
+        while ((line = br1.readLine()) != null)
+        {
+            String pattern = "[a-zA-Z]+:";
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
+            if (m.find())
+            {
+                tags.put(line.split(":")[0], contadorTags);
+            }
+            contadorTags++;
+        }
+        br1.close();
+        BufferedReader br2 = new BufferedReader(new FileReader(fileString));
+        while ((line = br2.readLine()) != null)
+        {
+            String codedInstruction = "";
+            String[] instruction = line.split(" ");
+            if (instruction[0].equals("lb"))
+            {
+               int int1 = Integer.parseInt(instruction[1]);
+               int int2 = Integer.parseInt(instruction[2]);
+               int int3 = Integer.parseInt(instruction[3]);
+               String dato1 = agregarCeros(Integer.toBinaryString(int1),4);
+               String dato2 = agregarCeros(Integer.toBinaryString(int2),4);
+               String dato3 = agregarCeros(Integer.toBinaryString(int3),4);
+               codedInstruction = addNops(arrayNops, int2, int3);
+               if (codedInstruction == "")
+               {
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               else
+               {
+                   arrayNops = new int[]{17,17,17};
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               codedInstruction = codedInstruction.concat("1000").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
+               codedFile.write(codedInstruction);
+               
+            }
+            if (instruction[0].equals("add"))
+            {
+               int int1 = Integer.parseInt(instruction[1]);
+               int int2 = Integer.parseInt(instruction[2]);
+               int int3 = Integer.parseInt(instruction[3]);
+               String dato1 = agregarCeros(Integer.toBinaryString(int1),4);
+               String dato2 = agregarCeros(Integer.toBinaryString(int2),4);
+               String dato3 = agregarCeros(Integer.toBinaryString(int3),4);
+               codedInstruction = addNops(arrayNops, int2, int3);
+               if (codedInstruction == "")
+               {
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               else
+               {
+                   arrayNops = new int[]{17,17,17};
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               codedInstruction = codedInstruction.concat("0001").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
+               codedFile.write(codedInstruction);
+            }
+            if (instruction[0].equals("sub"))
+            {
+               int int1 = Integer.parseInt(instruction[1]);
+               int int2 = Integer.parseInt(instruction[2]);
+               int int3 = Integer.parseInt(instruction[3]);
+               String dato1 = agregarCeros(Integer.toBinaryString(int1),4);
+               String dato2 = agregarCeros(Integer.toBinaryString(int2),4);
+               String dato3 = agregarCeros(Integer.toBinaryString(int3),4);
+               codedInstruction = addNops(arrayNops, int2, int3);
+               if (codedInstruction == "")
+               {
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               else
+               {
+                   arrayNops = new int[]{17,17,17};
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               codedInstruction = codedInstruction.concat("0010").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
+               codedFile.write(codedInstruction);
+            }
+            if (instruction[0].equals("mul"))
+            {
+               int int1 = Integer.parseInt(instruction[1]);
+               int int2 = Integer.parseInt(instruction[2]);
+               int int3 = Integer.parseInt(instruction[3]);
+               String dato1 = agregarCeros(Integer.toBinaryString(int1),4);
+               String dato2 = agregarCeros(Integer.toBinaryString(int2),4);
+               String dato3 = agregarCeros(Integer.toBinaryString(int3),4);
+               codedInstruction = addNops(arrayNops, int2, int3);
+               if (codedInstruction == "")
+               {
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               else
+               {
+                   arrayNops = new int[]{17,17,17};
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               codedInstruction = codedInstruction.concat("0011").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
+               codedFile.write(codedInstruction);
+            }
+            if (instruction[0].equals("bne"))
+            {
+               int int1 = Integer.parseInt(instruction[1]);
+               int int2 = Integer.parseInt(instruction[2]);
+               String dato1 = agregarCeros(Integer.toBinaryString(int1),4);
+               String dato2 = agregarCeros(Integer.toBinaryString(int2),4);
+               String dato3 = agregarCeros(Integer.toBinaryString(tags.get(instruction[3])),7);
+               codedInstruction = addNops(arrayNops, int1, int2);
+               codedInstruction = codedInstruction.concat("0100").concat(dato1).concat(dato2).concat(dato3).concat("\n");
+               codedInstruction = codedInstruction.concat("0000000000000000000\n0000000000000000000\n0000000000000000000\n");
+               codedFile.write(codedInstruction);
+               arrayNops = new int[]{17,17,17};
+            }
+            if (instruction[0].equals("addi"))
+            {
+               int int1 = Integer.parseInt(instruction[1]);
+               int int2 = Integer.parseInt(instruction[2]);
+               int int3 = Integer.parseInt(instruction[3]);
+               String dato1 = agregarCeros(Integer.toBinaryString(int1),4);
+               String dato2 = agregarCeros(Integer.toBinaryString(int2),4);
+               String dato3 = agregarCeros(Integer.toBinaryString(int3),4);
+               codedInstruction = addNops(arrayNops, int2, 16);
+               if (codedInstruction == "")
+               {
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               else
+               {
+                   arrayNops = new int[]{17,17,17};
+                   arrayNops = shiftArray(arrayNops, int1);
+               }
+               codedInstruction = codedInstruction.concat("1010").concat(dato1).concat(dato2).concat(dato3).concat("000\n");
+               codedFile.write(codedInstruction);
+            }
+        }
+        codedFile.close();
    } 
 }
